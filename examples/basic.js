c355ec5671073ee93ca9b4fb0685d1f4c760cc37
Charles Powell <cbpowell@gmail.com>
2013-10-15 19:51:49 -0700

    Reduced functionality to get core requirements needed

--- examples/basic.js
+++ examples/basic.js
@@ -5,29 +5,56 @@ var mumble = require('../');
 var fs = require('fs');
 
 var options = {
-    key: fs.readFileSync( 'private.pem' ),
-    cert: fs.readFileSync( 'public.pem' )
+	//username: 'mumbot',
+	//password: 'coolio'
+    //key: fs.readFileSync( 'private.pem' ),
+    //cert: fs.readFileSync( 'public.pem' )
+	pfx: fs.readFileSync( 'MumbleCert.p12' ),
 }
 
+var output;
+
 console.log( 'Connecting' );
-mumble.connect( 'mumble://example.org', options, function ( error, connection ) {
+mumble.connect( 'mumble://psyjnir.net/-%20Lobby', options, function ( error, connection ) {
     if( error ) { throw new Error( error ); }
 
     console.log( 'Connected' );
-
-    connection.authenticate( 'ExampleUser' );
-    connection.on( 'initialized', onInit );
-    connection.on( 'voice', onVoice );
+	
+	// Authenticate and initialize
+    connection.authenticate( 'mumbot', 'coolio' );
+    connection.on( 'initialized', function() {
+		output = connection;
+		console.log( 'Connection initialized' );
+	} );
+	
+	// Set up callbacks
+    connection.on( 'user-update', onUserUpdate );
+	connection.on( 'user-remove', onUserRemove );
 });
 
 var onInit = function() {
-    console.log( 'Connection initialized' );
-
+	
+	
     // Connection is authenticated and usable.
 };
+
+var onUserUpdate = function( user ) {
+	console.log( 'User update:', user );
+	console.log( 'Connection:', output);
+	//console.log( 'All users:', mumble.users)
+}
+
+var onUserRemove = function ( user ) {
+	console.log( 'User removed:', user );
+	output.sendMessage( 'UserState', {session: user.session, actor: user.session, channelId:5})
+}
 
 var onVoice = function( event ) {
     console.log( 'Mixed voice' );
 
     var pcmData = voice.data;
 }
+
+process.on('exit', function() {
+  console.log('About to exit.');
+});
